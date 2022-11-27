6364ab3c157358b55f89e900e4cfdca694f829c4
Athelas-Titan <94730858+Athelas-Titan@users.noreply.github.com>
2022-11-27 18:53:08 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -10,3 +10,13 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("cartBtn").onclick = function() {displayCart()};
+document.getElementById("exitCart").onclick = function() {hideCart()};
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function displayCart() {
+    document.getElementById("myCart").classList.add("show");
+}
+function hideCart() {
+    document.getElementById("myCart").classList.remove("show");
+}
